ee40c7490e81b8fca7a8848087d86f511211ea1f
raster <pete@rasterweb.net>
2010-11-14 07:09:43 -0600

    added missing file

---
+++ table-tracks.sql
@@ -0,0 +1,24 @@
+
+CREATE TABLE `tracks` (
+  `id` int(11) NOT NULL auto_increment,
+  `artist` varchar(255) NOT NULL default '',
+  `artistmbid` varchar(64) NOT NULL default '',
+  `mbid` varchar(64) NOT NULL default '',
+  `name` varchar(255) NOT NULL default '',
+  `streamable` varchar(10) default NULL,
+  `album` varchar(255) NOT NULL default '',
+  `albummbid` varchar(64) NOT NULL default '',
+  `urltrack` varchar(255) default NULL,
+  `urlartist` varchar(255) default NULL,
+  `imagesmall` varchar(255) default NULL,
+  `imagemedium` varchar(255) default NULL,
+  `imagelarge` varchar(255) default NULL,
+  `imageextralarge` varchar(255) default NULL,
+  `datestring` datetime NOT NULL default '0000-00-00 00:00:00',
+  `dateuts` varchar(64) NOT NULL default '',
+  `modified` timestamp NOT NULL default CURRENT_TIMESTAMP,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `dateuts` (`dateuts`)
+) ENGINE=MyISAM AUTO_INCREMENT=47322 DEFAULT CHARSET=utf8;
+
+
